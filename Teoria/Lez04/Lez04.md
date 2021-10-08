87941b2057f3896784cc02fcd94c7db08131503f
kevinm6 <72861758+kevinm6@users.noreply.github.com>
2021-10-08 11:52:50 +0200

    Updating fiels

---
+++ Teoria/Lez04/Lez04.md
@@ -0,0 +1,32 @@
+# Lez04
+
+---
+
+List <...>
+
+- Boxing | Unboxing
+- Generici
+
+---
+
+I/O
+> `System.out.println(...)`
+
+stream (STDIN) `System.in`\
+stream (STDOUT) `System.out`
+
+---
+
+```java
+public class Liste {
+	
+	public static void main(String[] args) {
+		List<Integer> l;
+	
+
+
+	}
+
+}
+
+```
---
+++ Teoria/Lez04/Liste.java
@@ -0,0 +1,17 @@
+// Liste java
+
+import java.util.List;
+import java.util.LinkedList;
+
+public class Liste {
+	
+	public static void main(String[] args) {
+		
+		List<Integer> lista = new LinkedList();
+		for (int i = 0; i < 10; i++) {
+			lista.add(i); // Integer.valueOf(i)
+		}	
+		System.out.println(lista);
+	}
+}
+
