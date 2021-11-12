425903e36fb71aaf8cf548bc534cf3d8d826c1aa
kevinm6 <72861758+kevinm6@users.noreply.github.com>
2021-11-12 11:29:27 +0100

    Upload new files

---
+++ Laboratorio/Lab05/IntQueue.java
@@ -0,0 +1,32 @@
+/**
+ *	OVERVIEW: Le istanze di questa classe rappresentano code di interi.
+ *						Una coda tipica è [x_1, x_2, ..., x_k] dove x_1 è il primo 
+ *						elemento inserito nella coda e x_k è l'ultimo
+ *						Dato che è una struttura dati con accesso FIFO,
+ *						dopo un'operazione di enqueue, la coda sarà [x_1, ..., x_k + 1] 
+ *						mentre
+ *						dopo un operazione di dequeue, la coda sarà [x_2, ..., x_k].
+ *
+ * */
+
+
+public class IntQueue {
+	// Attributi
+	// Array di interi che conterrà gli elementi della coda
+	int[] elems;
+	// Gli indici di testa e di coda.
+	// head è l'indice del primo elemento
+	// tail è l'indice della prima posizione nella quale effettuare un inserimento
+	int head, tail;
+
+	/**
+	 * Funzione di astrazione:
+	 *	AF()
+	 * 
+	 * */
+
+	// Costruttori
+
+	// Metodi
+
+}
