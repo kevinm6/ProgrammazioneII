e88aacd0d295be5772dfb483dfc4e61cc6edcb4f
kevinm6 <72861758+kevinm6@users.noreply.github.com>
2022-06-14 08:47:18 +0200

    Update repo

--- Teoria/Lez04/Liste.java
+++ Teoria/Lez04/Liste.java
@@ -1,12 +1,12 @@
 // Liste java
 
-import java.util.List;
 import java.util.LinkedList;
+import java.util.List;
 
 public class Liste {
 	
 	public static void main(String[] args) {
-		
+
 		List<Integer> lista = new LinkedList<>();
 		for (int i = 0; i < 10; i++) {
 			lista.add(i); // Integer.valueOf(i)
--- Teoria/Lez13/AbstractListPicker.java
+++ Teoria/Lez13/AbstractListPicker.java
@@ -1,10 +1,9 @@
-/* 
+/*
  * Kevin Manca
  * AbstractListPicker.java
  */
 
-
-package it.unimi.di.prog2.dalvivo;
+package List;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -12,20 +11,21 @@ import java.util.Objects;
 
 public abstract class AbstractListPicker {
 
-  final private List<String> candidates;
+  private final List<String> candidates;
 
-	public AbstractListPicker(final List<String> candidates) {
+  public AbstractListPicker(final List<String> candidates) {
     Objects.requireNonNull(candidates, "La lista di candidati non deve essere null.");
-    for (final String c: candidates) Objects.requireNonNull(c, "I candidati non devono essere null.");
+    for (final String c : candidates)
+      Objects.requireNonNull(c, "I candidati non devono essere null.");
     this.candidates = new LinkedList<>(candidates);
-	}
+  }
 
   /**
-  * Rimuove l'i-esimo elemento e lo restituisce
-  * 
-  * @param i
-  * @return
-  */
+   * Rimuove l'i-esimo elemento e lo restituisce
+   *
+   * @param i
+   * @return
+   */
   protected String remove(final int i) {
     return candidates.remove(i);
   }
@@ -34,10 +34,6 @@ public abstract class AbstractListPicker {
     return candidates.size();
   }
 
-  /**
-  * Restituisce uno dei candidati
-  */
+  /** Restituisce uno dei candidati */
   public abstract String pick();
-
 }
-
